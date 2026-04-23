5ec28e48fe0aeea8c1dbc56da3feb69573de2984
allancamaraln-alt <allancamaraln@gmail.com>
2026-04-23 15:25:47 -0300

    chore: remove evento de teste do Sentry

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ Sentry.init({
   tracesSampleRate: 1.0,
   environment: "production"
 })
-Sentry.captureMessage("ClimaPro: Sentry conectado com sucesso")
 
 const Dashboard    = lazy(() => import('./pages/Dashboard'))
 const Clientes     = lazy(() => import('./pages/Clientes'))
